fecae27fc1034ef838daceeb5f124f4a281e46ee
Harsh Maurya <harshx8878@gmail.com>
2026-03-12 23:51:12 +0530

    cors changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,13 @@ import {app , server} from "./lib/socket.js";
 const __dirname = path.resolve();
 
 let port = ENV.PORT;
-app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://chatify-frontend-vmj9.onrender.com"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser( ));
